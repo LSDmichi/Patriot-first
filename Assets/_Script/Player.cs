b54ba5bef745946af0c28dcc8e753235d2cf739f
LSDmichi <masamichi.higashi@icloud.com>
2018-06-09 18:03:55 +0900

    first_commit

---
+++ Assets/_Script/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public string name;
+	public uint public_money;
+	public uint private_money;
+	public int job_type;
+
+	public void action (uint public_money, uint private_money, int job_type){
+		
+	}
+}

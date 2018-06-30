619c28eaff4e3e65e79787448f5b0e0c42a41ece
LSDmichi <masamichi.higashi@icloud.com>
2018-06-30 17:41:36 +0900

    no error

--- Assets/_Script/Weight_List.cs
+++ Assets/_Script/Weight_List.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Weight_List {
 
-	public GameObject tile_object;
+	public Object tile_object;
 	public int weight;
 
-	public List<GameObject> InitList(){
+	public List<Object> InitList(){
 		List<Weight_List> tiles = new List<Weight_List>{
 			// 抽選割合、
 			new Weight_List{tile_object = Resources.Load("/_Prefab/desert"), 	weight = 2	},
@@ -18,7 +18,7 @@ public class Weight_List {
 			new Weight_List{tile_object = Resources.Load("/_Prefab/gold"), 		weight = 3	}
 		};
 
-		List<GameObject> tile_object_list = new List<GameObject>();
+		List<Object> tile_object_list = new List<Object>();
     	foreach(Weight_List tile in tiles){
        		for(int i = 1; i <= tile.weight; i++){
        			tile_object_list.Add(tile.tile_object);

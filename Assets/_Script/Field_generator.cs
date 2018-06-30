b7bc03e65b613136b3a47dcfda123569089cb3c4
LSDmichi <masamichi.higashi@icloud.com>
2018-06-30 15:47:49 +0900

    somefixes

--- Assets/_Script/Field_generator.cs
+++ Assets/_Script/Field_generator.cs
@@ -5,31 +5,34 @@ using UnityEngine;
 public class Field_generator : MonoBehaviour {
 
 	public int tile_max_count; // 最大タイル数
-	private int[] tile_list; // タイル
+	private List<GameObject> tile_object_list; // タイル
 	private Vector3 gen_pos; // 生成場所
 
 	// Use this for initialization
 	void Start () {
-		Vector3 gen_pos = new Vector3(1, 1, 1);
+		Vector3 gen_pos = new Vector3(0, 0, 0);
 		int tile_max_count = 100; // after everthig has done, we create more unique maps
+		Weight_List name_list = new Weight_List();
+		List<GameObject> tile_object_list = name_list.InitList();
 	}
 
-	// public void generate_field(Vector3 gen_pos) {
+	public void generate_field(Vector3 gen_pos) {
 
-	// 	for(int i = 0; i <= tile_max_count; i++) {
-	// 		GameObject tile = pick_tile_type(tile_list);
-	// 		Vector3 new_gen_pos = generate_position(gen_pos, i);
-	// 		Instantiate(tile, new_gen_pos);
-	// 	}
+		for(int i = 0; i <= tile_max_count; i++) {
+			GameObject tile = pick_tile_type(tile_object_list);
+			Vector3 new_gen_pos = generate_position(gen_pos, i);
+			Instantiate(tile, new_gen_pos, Quaternion.identity);
+		}
 
-	// }
+	}
 
-	// private GameObject pick_tile_type(int[] tile_list){
-	// 	int rand_int = Random.Range(0, 100);
-	// }
+	private GameObject pick_tile_type(List<GameObject> tile_object_list){
+		int rand_int = Random.Range(0, 100);
+    	return tile_object_list[rand_int];
+	}
 
-	// private Vector3 generate_position(Vector3 gen_pos, int i) {
-
-	// }
+	private Vector3 generate_position(Vector3 gen_pos, int i) {
+		return gen_pos;
+	}
 
 }
--- Assets/_Script/Weight_List.cs
+++ Assets/_Script/Weight_List.cs
@@ -2,22 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weight_List : MonoBehaviour {
+public class Weight_List {
 
-	public string tile_name;
+	public GameObject tile_object;
 	public int weight;
 
-	public List<Weight_List> InitList(){
+	public List<GameObject> InitList(){
 		List<Weight_List> tiles = new List<Weight_List>{
 			// 抽選割合、
-			new Weight_List{ tile_name = "desert",		weight = 2	},
-			new Weight_List{ tile_name = "wood",		weight = 10	},
-			new Weight_List{ tile_name = "oil", 		weight = 10	},
-			new Weight_List{ tile_name = "iron", 		weight = 10	},
-			new Weight_List{ tile_name = "food", 		weight = 15	},
-			new Weight_List{ tile_name = "gold", 		weight = 3	}
+			new Weight_List{tile_object = Resources.Load("/_Prefab/desert"), 	weight = 2	},
+			new Weight_List{tile_object = Resources.Load("/_Prefab/wood"),		weight = 10	},
+			new Weight_List{tile_object = Resources.Load("/_Prefab/oil"), 		weight = 10	},
+			new Weight_List{tile_object = Resources.Load("/_Prefab/iron"), 		weight = 10	},
+			new Weight_List{tile_object = Resources.Load("/_Prefab/food"), 		weight = 15	},
+			new Weight_List{tile_object = Resources.Load("/_Prefab/gold"), 		weight = 3	}
 		};
 
-		return tiles;
+		List<GameObject> tile_object_list = new List<GameObject>();
+    	foreach(Weight_List tile in tiles){
+       		for(int i = 1; i <= tile.weight; i++){
+       			tile_object_list.Add(tile.tile_object);
+    		}
+    	}
+
+		return tile_object_list;
 	}
 }

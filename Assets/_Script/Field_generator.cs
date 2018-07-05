9e5c4923b708db8f297cf1e417243c3916fda627
LSDmichi <masamichi.higashi@icloud.com>
2018-07-06 00:09:51 +0900

    fix

--- Assets/_Script/Field_generator.cs
+++ Assets/_Script/Field_generator.cs
@@ -13,22 +13,23 @@ public class Field_generator : MonoBehaviour {
 	void Start () {
 		Vector3 gen_pos = new Vector3(-45, 1, 45);
 		int tile_max_count = 100; // after everthig has done, we create more unique maps
-		Weight_List name_list = new Weight_List();
-		List<Object> tile_object_list = name_list.InitList();
+		//Weight_List name_list = new Weight_List();
+		//List<Object> tile_object_list = name_list.InitList();
 	}
 
-	public void generate_field(Vector3 gen_pos) {
+	public void generate_field(){
+        Weight_List name_list = new Weight_List();
+		List<Object> tile_object_list = name_list.InitList();
 
 		for(int i = 0; i <= tile_max_count; i++) {
 			Object tile = pick_tile_type(tile_object_list);
 			new_gen_pos = generate_position(gen_pos, i);
 			Instantiate(tile, new_gen_pos, Quaternion.identity);
 		}
-
 	}
 
 	private Object pick_tile_type(List<Object> tile_object_list){
-		int rand_int = Random.Range(0, 100);
+		int rand_int = Random.Range(0, 50);
 		Object tile = tile_object_list[rand_int];
     	return tile;
 	}

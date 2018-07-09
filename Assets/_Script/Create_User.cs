478f406c067c22934a966441f2d1f25cfefc39f6
LSDmichi <masamichi.higashi@icloud.com>
2018-07-10 01:23:47 +0900

    add New User Scene

---
+++ Assets/_Script/Create_User.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Create_User : MonoBehaviour {
+
+    [SerializeField]
+    Text user_name;
+
+    [SerializeField]
+    GameObject create_btn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!string.IsNullOrEmpty(user_name.text))
+        {
+            create_btn.SetActive(true);
+        }
+        else
+        {
+            create_btn.SetActive(false);
+        }
+	}
+}
